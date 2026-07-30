40597a053ad691e730209cc05429f9394c6e0f9d
mdaneri <17148649+mdaneri@users.noreply.github.com>
2026-07-30 12:08:16 -0700

    Synchronize NTP asset cache references

--- atlaso/app/static/service-worker.js
+++ atlaso/app/static/service-worker.js
@@ -5,7 +5,7 @@ const ATLASO_ASSETS = [
   "/static/offline.html",
   "/static/app.css?v=atlaso-wizard-monaco-20260729-11",
   "/static/ui-patterns.js?v=atlaso-ui-foundation-20260726-5",
-  "/static/app.js?v=atlaso-kickstart-variables-20260729-16",
+  "/static/app.js?v=atlaso-ntp-upstream-edit-20260730-17",
   "/static/terminal.js?v=web-terminal-review-20260716-3",
   "/static/vendor/xterm/xterm.css?v=5.5.0",
   "/static/vendor/xterm/xterm.js?v=5.5.0",
